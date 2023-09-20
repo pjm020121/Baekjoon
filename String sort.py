9b89a4c08c275598eed8bf1c6623c1a793750a6f
pjm020121 <pjm020121@naver.com>
2023-09-20 23:36:07 +0900

    Update

---
+++ String sort.py
@@ -0,0 +1,17 @@
+# 1181 / String sort
+
+import sys
+
+count = int(sys.stdin.readline())
+
+str_list = []
+small = []
+min = 0
+
+for i in range(count):
+
+    string = sys.stdin.readline()
+    str_list.append(string)
+
+
+
