c18a8c0d0eb72a3b581848cd7985acd7c8b28df4
pjm020121 <pjm020121@naver.com>
2023-09-22 11:35:08 +0900

    Polor Bear

--- Polar Bear.py
+++ Polar Bear.py
@@ -14,6 +14,7 @@ for i in range(N):
 O = 0    # 열린괄호 횟수
 day = 1    # 걸린 날짜
 plus_day = 1    # 추가 날짜
+min_day = -1
 
 # 날짜 분석
 for i in range(len(s)):
@@ -26,11 +27,23 @@ for i in range(len(s)):
 
         O -= 1
 
-    # 괄호 안에 문자열 추가 시 날짜추가
+    # O가 - 상태일 때 비교해줄 변수
+    plus_O = +abs(O)
+
+    # 괄호 안에 O의 수를 계산해서 날짜추가
     if O > plus_day:
 
         plus_day += 1
         day += 1
+
+    # 괄호 안에 X의 수를 계산해서 날짜추가
+    if O < min_day:
+
+        if plus_O > plus_day:
+
+            min_day -= 1
+            plus_day += 1
+            day += 1
 
 # 괄호가 다 닫히지 않았을 때
 if O != 0:
