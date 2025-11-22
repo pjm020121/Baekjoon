f2fb83ca06c6225f3a617a3a8b63a1a6c7484e02
pjm020121 <140699993+pjm020121@users.noreply.github.com>
2025-11-22 09:41:41 +0000

    Update

--- Python/(ing)Printer_Queue - 1966.py
+++ Python/(ing)Printer_Queue - 1966.py
@@ -6,7 +6,7 @@ for i in range(Testcase_num):
 
     testcase = list(map(int, sys.stdin.readline().split()))
     Printer_Queue = list(map(int, sys.stdin.readline().split()))
-    pointer = -1
+    pointer = 0
     count = 0
 
     if testcase[0] == 1:
@@ -16,30 +16,46 @@ for i in range(Testcase_num):
     elif testcase[0] > 1:
 
         for k in range(testcase[0]):
-
-            if pointer == testcase[1]:
-                break
-
+            
+            print(Printer_Queue, "\n", pointer, "\n", testcase[1])
+            
             count += 1
 
-            pointer = (pointer + 1) % len(Printer_Queue)
+            if (pointer == testcase[1] and max_val >= max(Printer_Queue)) or len(Printer_Queue) == 1:
+                break
+    
+            if Printer_Queue[pointer] < max(Printer_Queue):
+                
+                max_ind = Printer_Queue.index(max(Printer_Queue))
+                max_val = max(Printer_Queue)
+                Printer_Queue.pop(max_ind)
 
-            if Printer_Queue[pointer] < max(Printer_Queue) and Printer_Queue[pointer] != 0:
+                if max_ind > len(Printer_Queue) -1:
+                    max_ind -= 1
 
                 if pointer == testcase[1]:
-                    testcase[1] = len(Printer_Queue)
+                    testcase[1] = len(Printer_Queue) - 1
 
-                pop = Printer_Queue[pointer]
-                Printer_Queue.append(pop)
-
-                if len(Printer_Queue) - 1 > testcase[1]:
-
+                elif pointer < testcase[1]:
                     testcase[1] -= 1
 
-                pointer = Printer_Queue.index(max(Printer_Queue))
+                if max_ind > pointer:
+                    max_ind -= 1
 
-            Printer_Queue[pointer] = 0
+                pop = Printer_Queue.pop(pointer)
+                Printer_Queue.append(pop)
 
+                pointer = max_ind
+            
+            else:
+                max_val = Printer_Queue.pop(pointer)
+
+                if pointer < testcase[1]:
+                    testcase[1] -= 1
+
+                if pointer > len(Printer_Queue) -1:
+                    pointer -= 1
+            
         print(count)
 
 
